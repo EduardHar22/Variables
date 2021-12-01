8eef6d0b0cba43296baa1e450117e6f90dd78aad
Eduard Harutyunyan <itm.eduard.harutyunyan@gmail.com>
2021-12-01 14:26:32 +0400

    Added project.js file and did tasks

---
+++ project.js
@@ -0,0 +1,28 @@
+// let name, admin;
+// name = "John";
+// admin = name;
+// console.log(admin)
+//the output of admin will be "John"
+
+// let i = 10;
+// let a = i++ % 5;
+// console.log(a)
+//the output will be 0
+
+// let a = 10;
+// let b = 3;
+// let c = a;
+// a = b;
+// b = c;
+// console.log("a = " + a, "b = " + b);
+// the output of a will be 3 and the output of b will be 10
+
+// let a = 10;
+// let b = 3;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log("a = " + a,"b = " + b);
+
+
+
